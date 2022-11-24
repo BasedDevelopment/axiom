2938396a95ce4c0428f19851e31bb934d4527d55
Luke <lukewhrit@pm.me>
2022-11-24 17:10:54 -0500

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  import '../app.css';
+</script>
+
+<div class="py-8 px-80 mx-auto">
+  <nav class="border-b p-3 mb-5 border-black flex justify-between text-lg">
+    <a href="https://as206628.net" class="font-bold hover:underline">🦊 EricNet</a>
+    <div>
+      <span class="text-blue-500 hover:bg-blue-100 p-2 rounded-md hover:cursor-pointer">jupiter</span>
+    </div>
+  </nav>
+  <main>
+    <slot />
+  </main>
+</div>
---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,51 @@
+<header class="flex justify-between align-middle">
+  <div class="font-mono">
+    <p class="text-3xl font-semibold">jupiter</p>
+    <p class="text-md">box0 / 134.195.121.106</p>
+  </div>
+  <div class="place-self-center">
+    <p class="font-bold text-white bg-emerald-500 px-8 py-2 rounded-md">
+      ON
+    </p>
+  </div>
+</header>
+
+<main class="grid grid-cols-8  mt-5">
+  <section class="col-span-2">
+    <ul class="block py-2 mr-10 rounded-md bg-gray-100 text-black shadow-sm">
+      <li class="my-1 mx-3 font-semibold">
+        <a
+          href="/app/"
+          class="py-2 px-4 flex gap-3 hover:bg-gray-300 rounded-md"
+        >
+          Stop / Start
+        </a>
+      </li>
+      <li class="my-1 mx-3 font-semibold">
+        <a
+          href="/app/"
+          class="py-2 px-4 flex gap-3 hover:bg-gray-300 rounded-md"
+        >
+          Reboot
+        </a>
+      </li>
+      <li class="my-1 mx-3 font-semibold">
+        <a
+          href="/app/"
+          class="py-2 px-4 flex gap-3 hover:bg-gray-300 rounded-md"
+        >
+          Hibernate
+        </a>
+      </li>
+      <li class="my-1 mx-3 font-semibold">
+        <a
+          href="/app/"
+          class="py-2 px-4 flex gap-3 hover:bg-gray-300 rounded-md"
+        >
+          Reset
+        </a>
+      </li>
+    </ul>
+  </section>
+  <section class="col-span-6 bg-slate-900 rounded h-96"></section>
+</main>
