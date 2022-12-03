612f4b28e183c757c3e9ca6765180c52d4263193
Luke <lukewhrit@pm.me>
2022-12-03 02:32:49 -0500

    feat: dark mode switcher (still needs styling)

---
+++ src/lib/components/sidebar/Navbar.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+  import theme from '$lib/shared/stores/theme';
+  import { Menu, MenuButton, MenuItem, MenuItems } from '@rgossiaux/svelte-headlessui';
+
+  function handleClick() {
+    document.getElementById('sidebar-container')?.classList.toggle('hidden')
+  }
+
+  function classNames(...classes: (string | false | null | undefined)[]) {
+    return classes.filter(Boolean).join(" ");
+  }
+
+  function resolveClass({
+    active,
+    disabled,
+  }: {
+    active: boolean;
+    disabled: boolean;
+  }) {
+    return classNames(
+      'flex gap-2 w-full px-4 py-2 text-sm leading-5 text-left',
+      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+      disabled && 'cursor-not-allowed opacity-50'
+    );
+  }
+
+</script>
+
+<header class="flex w-full items-center justify-between bg-slate-800 text-gray-300 p-4">
+  <div class="flex items-center space-x-2">
+    <button type="button" class="text-3xl" on:click={handleClick}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+        />
+      </svg>
+    </button>
+    <div class="px-4">
+      <a href="/" class="text-lg font-semibold">🦊 EricNet</a>
+    </div>
+  </div>
+  <div class="flex gap-3">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
+      />
+    </svg>
+
+    <Menu>
+      <MenuButton>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+        </svg>
+      </MenuButton>
+
+      <MenuItems
+        class="absolute right-3 w-36 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
+      >
+        <div class="py-1">
+          <MenuItem as="button" on:click={() => theme.set('light')} class={resolveClass}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+            </svg>
+            <span>Light</span>
+          </MenuItem>
+          <MenuItem as="button" on:click={() => theme.set('dark')} class={resolveClass}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+            </svg>
+            <span>Dark</span>
+          </MenuItem>
+          <MenuItem as="button" on:click={() => theme.set('system')} class={resolveClass}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25" />
+            </svg>
+            <span>System</span>
+          </MenuItem>
+        </div>
+      </MenuItems>
+    </Menu>
+  </div>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,33 @@
 <script lang="ts">
   import '../app.css';
 
+  import Sidebar from '$lib/components/sidebar/Sidebar.svelte';
   import Section from '$lib/components/sidebar/Section.svelte';
   import Action from '$lib/components/sidebar/Action.svelte';
   import SubAction from '$lib/components/sidebar/SubAction.svelte';
+  import Navbar from '$lib/components/sidebar/Navbar.svelte';
 
   import { page } from '$app/stores';
-  import type { PageData } from './$types';
-  import Sidebar from '$lib/components/sidebar/Sidebar.svelte';
   import { firstUpperCase } from '$lib/firstUpperCase';
-
-  function handleClick() {
-    document.getElementById('sidebar-container')?.classList.toggle('hidden')
-  }
+  import theme from '$lib/shared/stores/theme';
+  import type { PageData } from './$types';
 
   $: activeUrl = $page.url.pathname;
   $: activeHash = $page.url.hash;
+
+  function isDark() {
+    if (import.meta.env.SSR) {
+      return false;
+    }
+
+    return $theme === 'dark' || ($theme === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches);
+  }
+
+  theme.subscribe(() => {
+    currentTheme = isDark() ? 'dark' : '';
+  })
+
+  $: currentTheme = isDark() ? 'dark' : '';
 
   export let data: PageData;
 </script>
@@ -25,63 +37,8 @@
   <meta name="description" content="Web interface for Eve">
 </svelte:head>
 
-<main class="min-h-screen w-full">
-  <!-- navbar -->
-  <header class="flex w-full items-center justify-between bg-slate-800 text-gray-300 p-4">
-    <!-- logo -->
-    <div class="flex items-center space-x-2">
-      <button type="button" class="text-3xl" on:click={handleClick}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
-      </button>
-      <div class="px-4">
-        <a href="/" class="text-lg font-semibold">🦊 EricNet</a>
-      </div>
-    </div>
-
-    <div class="flex gap-3">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-6 h-6"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
-        />
-      </svg>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-6 h-6"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-        />
-      </svg>
-    </div>
-  </header>
+<main class="min-h-screen w-full {currentTheme}">
+  <Navbar />
 
   <div class="flex h-full" style="min-height: 93.3vh;">
     <div id="sidebar-container">
@@ -92,8 +49,7 @@
         {#each data.boxes as box}
           <Section name={box.name}>
             {#each box.vms as server}
-              <Action href="/{server.id}" active={false}>{firstUpperCase(server.name)}</Action
-              >
+              <Action href="/{server.id}" active={false}>{firstUpperCase(server.name)}</Action>
               {#if activeUrl === `/${server.id}`}
                 <SubAction href="/{server.id}" active={activeHash !== '#statistics'}
                   >Console</SubAction
