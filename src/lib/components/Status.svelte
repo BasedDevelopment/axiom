937f9185810b2382ac9ac2316e19be542203acee
Luke <lukewhrit@pm.me>
2023-01-18 20:38:32 -0500

    fix(status): use complete class names for status background color

--- src/lib/components/Status.svelte
+++ src/lib/components/Status.svelte
@@ -14,20 +14,20 @@
   };
 
   const colors = [
-    'gray-400', // Unknown
-    'emerald-500', // On
-    'amber-400', // Blocked
-    'gray-400', // Paused
-    'rose-500', // Off, Shutdown
-    'rose-500', // Off, Shutoff
-    'gray-400', // Crashed
-    'neutral-900', // Suspended
+    'bg-gray-400', // Unknown
+    'bg-emerald-500', // On
+    'bg-amber-400', // Blocked
+    'bg-gray-400', // Paused
+    'bg-rose-500', // Off, Shutdown
+    'bg-rose-500', // Off, Shutoff
+    'bg-gray-400', // Crashed
+    'bg-neutral-900', // Suspended
   ];
 </script>
 
 <div class="inline-block text-center text-sm">
   <span class="sr-only">VM Status:</span>
-  <p title={reason} class={`font-bold text-white bg-${colors[0] || 'gray-400'} p-1 rounded-md`}>
+  <p title={reason} class="font-bold text-white {colors[code]} p-1 rounded-md">
     {Status[code].toUpperCase()}
   </p>
 </div>
