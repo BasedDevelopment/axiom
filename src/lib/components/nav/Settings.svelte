a590cb26a51d571d20aaebf1417c5a7326029f49
Luke <lukewhrit@pm.me>
2023-01-19 23:32:31 -0500

    feat(auth): logout button

--- src/lib/components/nav/Settings.svelte
+++ src/lib/components/nav/Settings.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
+  import cookie from 'cookie';
   import { Dialog, DialogOverlay, DialogTitle, Transition } from '@rgossiaux/svelte-headlessui';
+  import { goto } from '$app/navigation';
 
   export let isOpen: boolean = false;
+
+  async function logout() {
+    const date = new Date();
+    date.setDate(date.getDate() - 1);
+
+    document.cookie = cookie.serialize('token', '', {
+      expires: date,
+      sameSite: 'strict',
+    });
+
+    await goto('/');
+  }
 </script>
 
 <Transition
@@ -23,7 +37,7 @@
       <DialogTitle class="bg-indigo-600 text-white font-bold py-3 px-8">Settings</DialogTitle>
 
       <div class="py-4 px-8 bg-none">
-        <button> Log out </button>
+        <button on:click={logout}>Log out</button>
       </div>
     </div>
   </Dialog>
